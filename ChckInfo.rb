8ec1995b7bf555b60699322a1e52fd05b606eca5
21o1 <80163540+21o1@users.noreply.github.com>
2022-06-12 02:43:14 +0300

    Add files via upload

---
+++ ChckInfo.rb
@@ -0,0 +1,22 @@
+uu = open("hell.txt", "r").read()
+ui = uu.length
+u8 = uu.to_s
+uri = []
+oa = 0 
+ui2 = ui/3
+r = 0 
+r1 =1
+print ui2
+#ui2.times do |uu1|
+#    fry = u8[oa].to_s + u8[oa+1].to_s+ u8[oa+2].to_s
+#    print ">>>> #{fry} <<<< Under Check {#{r1}/#{ui2}} #{r}s\r"
+#    sleep 0.7
+#    if fry == "htt"
+#        uri.append(1)
+#    end
+#    r += 0.7
+#    r1 +=1 
+#    oa +=1
+#end
+#puts ""
+#puts uri.to_s + " \nFound In File[+]"
