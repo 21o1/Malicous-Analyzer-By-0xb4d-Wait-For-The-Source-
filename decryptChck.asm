2a7084c1cc89025e293d7de1eccbb9efdbb237da
21o1 <80163540+21o1@users.noreply.github.com>
2022-06-14 04:14:10 +0300

    Update decryptChck.asm

---
+++ decryptChck.asm
@@ -0,0 +1,21 @@
+section .data
+mov eax,2023
+
+global _start
+
+section .text
+
+_start:
+  mov rax, 1        
+  mov rdi, 1        
+  mov rsi, msg     
+  mov rdx, msglen   
+  syscall           
+
+  mov rax, 60       
+  mov rdi, 0        
+  syscall           
+
+section .rodata
+  msg: db "Wait For It", 10
+  msglen: equ $ - msg
